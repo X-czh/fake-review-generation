39fe739630e4a3e8b945927486b7ba0104693245
X-czh <m1a2@outlook.com>
2018-07-14 15:32:38 -0400

    update data preprocessing

---
+++ data/dataset_split.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+if __name__ == '__main__':
+
+    # Read the CSV file to pandas dataframe
+    df = pd.read_csv('restaurant_review.csv', delimiter='\t')
+
+    # Shuffle the dataframe
+    df = df.sample(frac=1.0).reset_index(drop=True)
+
+    # Split and save to CSV
+    train_file = df[:180000].drop(['Unnamed: 0'], axis=1)
+    train_file.to_csv('train.csv', sep='\t', encoding='utf-8')
+    val_file = df[180000:190000].drop(['Unnamed: 0'], axis=1)
+    val_file.to_csv('val.csv', sep='\t', encoding='utf-8')
+    test_file = df[190000:200000].drop(['Unnamed: 0'], axis=1)
+    test_file.to_csv('test.csv', sep='\t', encoding='utf-8')
--- data/preprocess.py
+++ data/preprocess.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 
 if __name__ == '__main__':
+
     # Read the two CSV files to pandas dataframes
     df_business = pd.read_csv('business.csv')
     df_review = pd.read_csv('review.csv')
 
     # Delete unneeded keys in df_business
-    preserved_keys_business = ['business_id', 'name', 'city', 'state', 'categories']
+    preserved_keys_business = ['business_id', 'name', 'categories']
     for key in df_business.keys():
         if key not in preserved_keys_business:
             del df_business[key]
@@ -36,13 +37,18 @@ if __name__ == '__main__':
     # Remove any non-ASCII characters
     combo.replace({r'[^\x00-\x7F]+': ''}, regex=True, inplace=True)
 
+    # Lowercase all the letters
+    combo['name'] = combo['name'].astype(str).str.lower()
+    combo['categories'] = (combo['categories'].astype(str).str.lower()).astype(list)
+    combo['text'] = combo['text'].astype(str).str.lower()
+
     # Remove the new line characters in reviews
     combo.replace({r'\n+': ''}, regex=True, inplace=True)
 
     # Add space between alphanumerical characters and punctuation marks
-    combo.replace({r'([a-zA-Z0-9])([,:;.!?"()])': r'\1 \2'}, regex=True, inplace=True)
-    combo.replace({r'([,:;.!?"()])([a-zA-Z0-9])': r'\1 \2'}, regex=True, inplace=True)
-    
+    combo.replace({r'([a-z0-9])([,:;.!?"()])': r'\1 \2'}, regex=True, inplace=True)
+    combo.replace({r'([,:;.!?"()])([a-z0-9])': r'\1 \2'}, regex=True, inplace=True)
+
     # Save to CSV
-    save_file = combo.head(500000)
+    save_file = combo.head(200000)
     save_file.to_csv('restaurant_review.csv', sep='\t', encoding='utf-8')
